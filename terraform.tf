f028c87045c888778f13d40da3be8517f9d2c8ef
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-03 18:31:57 +0530

    feat: create basic main files

---
+++ terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  cloud {
+
+    organization = "ZeroCloud"
+
+    workspaces {
+      name = "End-to-End-Diagnostic-Data-Processing-and-Visualization-Using-AWS-Comprehend-Medical-Terraform-and-QuickSight"
+    }
+  }
+}
