2531425812f96877ca3e1cbfc2a9618a6e662780
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-04 10:28:58 +0530

    feat: create function infra and dynamodb trigger

---
+++ modules/dynamodb_stream_lambda_function/main.tf
@@ -0,0 +1,15 @@
+resource "aws_lambda_function" "dynamodb_trigger_lambda" {
+  function_name = var.function_name
+  role          = var.role_arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+
+  filename         = "${path.module}/function.zip"
+  source_code_hash = filebase64sha256("${path.module}/function.zip")
+}
+
+resource "aws_lambda_event_source_mapping" "dynamodb_trigger" {
+  event_source_arn  = var.source_arn
+  function_name     = aws_lambda_function.dynamodb_trigger_lambda.arn
+  starting_position = "LATEST"
+}
