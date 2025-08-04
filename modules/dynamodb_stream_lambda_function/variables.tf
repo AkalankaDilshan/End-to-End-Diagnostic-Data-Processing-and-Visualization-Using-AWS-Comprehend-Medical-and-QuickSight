00d11acef1fdc06004dda823f1249a0e767eff06
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-04 10:29:38 +0530

    feat: create nessesary input variables

---
+++ modules/dynamodb_stream_lambda_function/variables.tf
@@ -0,0 +1,15 @@
+variable "function_name" {
+  description = "lambda function name"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "lambda function role arn"
+  type        = string
+}
+
+variable "source_arn" {
+  description = "trigger source arn"
+  type        = string
+}
+
