bfe9c44d2b8a63655a2b0e8ce99760e70b315edc
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-04 13:31:56 +0530

    feat: python runtime issue

--- modules/dynamodb_stream_lambda_function/main.tf
+++ modules/dynamodb_stream_lambda_function/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "dynamodb_trigger_lambda" {
   function_name = var.function_name
   role          = var.role_arn
   handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
+  runtime       = "python3.11"
 
   filename         = "${path.module}/function.zip"
   source_code_hash = filebase64sha256("${path.module}/function.zip")
