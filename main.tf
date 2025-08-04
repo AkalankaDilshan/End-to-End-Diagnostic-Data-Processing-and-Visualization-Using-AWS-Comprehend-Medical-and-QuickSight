8be0dbef247e019604bc8afd7328b212f01ac007
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-04 11:58:58 +0530

    feat: call lambda,dynamodb and iam

--- main.tf
+++ main.tf
@@ -2,7 +2,21 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+
+module "iam_role_for_dynamodb_lambda" {
+  source    = "./modules/Iam/iam_for_dynamodb_trigger"
+  role_name = "dynamodb_lambda_iam_role"
+}
 module "dynamodb" {
   source     = "./modules/dynamodb"
   table_name = "Medical_report"
 }
+
+module "sns_trigger_lambda" {
+  source        = "./modules/dynamodb_stream_lambda_function"
+  function_name = "dynamodb_sns_function"
+  role_arn      = module.iam_role_for_dynamodb_lambda.function_role_arn
+  source_arn    = module.dynamodb.event_source_arn
+}
+
+
