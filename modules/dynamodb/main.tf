4dc1df26251f2dc3f52e8b5a14e4b4f09036ab6c
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-05 14:52:28 +0530

    chor: comment item section

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -14,111 +14,111 @@ resource "aws_dynamodb_table" "medical_reports" {
   }
 }
 
-# Item 1
-resource "aws_dynamodb_table_item" "report_item_1" {
-  table_name = aws_dynamodb_table.medical_reports.name
-  hash_key   = "Report_ID"
+# # Item 1
+# resource "aws_dynamodb_table_item" "report_item_1" {
+#   table_name = aws_dynamodb_table.medical_reports.name
+#   hash_key   = "Report_ID"
 
-  item = jsonencode({
-    "Report_ID"    = { "S" = "MR1000" },
-    "Patient_Name" = { "S" = "Chris Brown" },
-    "Age"          = { "N" = "60" },
-    "Gender"       = { "S" = "Male" },
-    "Test_Name"    = { "S" = "ECG" },
-    "Result"       = { "S" = "Requires Follow-up" },
-    "Date"         = { "S" = "2025-02-04" },
-    "Doctor"       = { "S" = "Dr. Taylor" },
-    "Remarks"      = { "S" = "Lifestyle changes recommended" }
-  })
-}
+#   item = jsonencode({
+#     "Report_ID"    = { "S" = "MR1000" },
+#     "Patient_Name" = { "S" = "Chris Brown" },
+#     "Age"          = { "N" = "60" },
+#     "Gender"       = { "S" = "Male" },
+#     "Test_Name"    = { "S" = "ECG" },
+#     "Result"       = { "S" = "Requires Follow-up" },
+#     "Date"         = { "S" = "2025-02-04" },
+#     "Doctor"       = { "S" = "Dr. Taylor" },
+#     "Remarks"      = { "S" = "Lifestyle changes recommended" }
+#   })
+# }
 
-# Item 2
-resource "aws_dynamodb_table_item" "report_item_2" {
-  table_name = aws_dynamodb_table.medical_reports.name
-  hash_key   = "Report_ID"
+# # Item 2
+# resource "aws_dynamodb_table_item" "report_item_2" {
+#   table_name = aws_dynamodb_table.medical_reports.name
+#   hash_key   = "Report_ID"
 
-  item = jsonencode({
-    "Report_ID"    = { "S" = "MR1001" },
-    "Patient_Name" = { "S" = "James Thomas" },
-    "Age"          = { "N" = "63" },
-    "Gender"       = { "S" = "Female" },
-    "Test_Name"    = { "S" = "Kidney Function Test" },
-    "Result"       = { "S" = "Healthy" },
-    "Date"         = { "S" = "2025-05-14" },
-    "Doctor"       = { "S" = "Dr. Williams" },
-    "Remarks"      = { "S" = "Further tests advised" }
-  })
-}
+#   item = jsonencode({
+#     "Report_ID"    = { "S" = "MR1001" },
+#     "Patient_Name" = { "S" = "James Thomas" },
+#     "Age"          = { "N" = "63" },
+#     "Gender"       = { "S" = "Female" },
+#     "Test_Name"    = { "S" = "Kidney Function Test" },
+#     "Result"       = { "S" = "Healthy" },
+#     "Date"         = { "S" = "2025-05-14" },
+#     "Doctor"       = { "S" = "Dr. Williams" },
+#     "Remarks"      = { "S" = "Further tests advised" }
+#   })
+# }
 
-# Item 3
-resource "aws_dynamodb_table_item" "report_item_3" {
-  table_name = aws_dynamodb_table.medical_reports.name
-  hash_key   = "Report_ID"
+# # Item 3
+# resource "aws_dynamodb_table_item" "report_item_3" {
+#   table_name = aws_dynamodb_table.medical_reports.name
+#   hash_key   = "Report_ID"
 
-  item = jsonencode({
-    "Report_ID"    = { "S" = "MR1002" },
-    "Patient_Name" = { "S" = "Emily Davis" },
-    "Age"          = { "N" = "47" },
-    "Gender"       = { "S" = "Female" },
-    "Test_Name"    = { "S" = "ECG" },
-    "Result"       = { "S" = "Healthy" },
-    "Date"         = { "S" = "2025-07-14" },
-    "Doctor"       = { "S" = "Dr. Taylor" },
-    "Remarks"      = { "S" = "Further tests advised" }
-  })
-}
+#   item = jsonencode({
+#     "Report_ID"    = { "S" = "MR1002" },
+#     "Patient_Name" = { "S" = "Emily Davis" },
+#     "Age"          = { "N" = "47" },
+#     "Gender"       = { "S" = "Female" },
+#     "Test_Name"    = { "S" = "ECG" },
+#     "Result"       = { "S" = "Healthy" },
+#     "Date"         = { "S" = "2025-07-14" },
+#     "Doctor"       = { "S" = "Dr. Taylor" },
+#     "Remarks"      = { "S" = "Further tests advised" }
+#   })
+# }
 
-# Item 4
-resource "aws_dynamodb_table_item" "report_item_4" {
-  table_name = aws_dynamodb_table.medical_reports.name
-  hash_key   = "Report_ID"
+# # Item 4
+# resource "aws_dynamodb_table_item" "report_item_4" {
+#   table_name = aws_dynamodb_table.medical_reports.name
+#   hash_key   = "Report_ID"
 
-  item = jsonencode({
-    "Report_ID"    = { "S" = "MR1003" },
-    "Patient_Name" = { "S" = "Sophia Anderson" },
-    "Age"          = { "N" = "45" },
-    "Gender"       = { "S" = "Female" },
-    "Test_Name"    = { "S" = "Blood Glucose" },
-    "Result"       = { "S" = "Healthy" },
-    "Date"         = { "S" = "2025-02-19" },
-    "Doctor"       = { "S" = "Dr. Taylor" },
-    "Remarks"      = { "S" = "No immediate concern" }
-  })
-}
+#   item = jsonencode({
+#     "Report_ID"    = { "S" = "MR1003" },
+#     "Patient_Name" = { "S" = "Sophia Anderson" },
+#     "Age"          = { "N" = "45" },
+#     "Gender"       = { "S" = "Female" },
+#     "Test_Name"    = { "S" = "Blood Glucose" },
+#     "Result"       = { "S" = "Healthy" },
+#     "Date"         = { "S" = "2025-02-19" },
+#     "Doctor"       = { "S" = "Dr. Taylor" },
+#     "Remarks"      = { "S" = "No immediate concern" }
+#   })
+# }
 
-# Item 5
-resource "aws_dynamodb_table_item" "report_item_5" {
-  table_name = aws_dynamodb_table.medical_reports.name
-  hash_key   = "Report_ID"
+# # Item 5
+# resource "aws_dynamodb_table_item" "report_item_5" {
+#   table_name = aws_dynamodb_table.medical_reports.name
+#   hash_key   = "Report_ID"
 
-  item = jsonencode({
-    "Report_ID"    = { "S" = "MR1004" },
-    "Patient_Name" = { "S" = "James Thomas" },
-    "Age"          = { "N" = "25" },
-    "Gender"       = { "S" = "Male" },
-    "Test_Name"    = { "S" = "Lipid Profile" },
-    "Result"       = { "S" = "Slightly Elevated" },
-    "Date"         = { "S" = "2025-04-05" },
-    "Doctor"       = { "S" = "Dr. Brown" },
-    "Remarks"      = { "S" = "Further tests advised" }
-  })
-}
+#   item = jsonencode({
+#     "Report_ID"    = { "S" = "MR1004" },
+#     "Patient_Name" = { "S" = "James Thomas" },
+#     "Age"          = { "N" = "25" },
+#     "Gender"       = { "S" = "Male" },
+#     "Test_Name"    = { "S" = "Lipid Profile" },
+#     "Result"       = { "S" = "Slightly Elevated" },
+#     "Date"         = { "S" = "2025-04-05" },
+#     "Doctor"       = { "S" = "Dr. Brown" },
+#     "Remarks"      = { "S" = "Further tests advised" }
+#   })
+# }
 
-# Item 6
-resource "aws_dynamodb_table_item" "report_item_6" {
-  table_name = aws_dynamodb_table.medical_reports.name
-  hash_key   = "Report_ID"
+# # Item 6
+# resource "aws_dynamodb_table_item" "report_item_6" {
+#   table_name = aws_dynamodb_table.medical_reports.name
+#   hash_key   = "Report_ID"
 
-  item = jsonencode({
-    "Report_ID"    = { "S" = "MR1005" },
-    "Patient_Name" = { "S" = "John Doe" },
-    "Age"          = { "N" = "56" },
-    "Gender"       = { "S" = "Female" },
-    "Test_Name"    = { "S" = "Blood Glucose" },
-    "Result"       = { "S" = "Normal" },
-    "Date"         = { "S" = "2025-03-05" },
-    "Doctor"       = { "S" = "Dr. Williams" },
-    "Remarks"      = { "S" = "Lifestyle changes recommended" }
-  })
-}
+#   item = jsonencode({
+#     "Report_ID"    = { "S" = "MR1005" },
+#     "Patient_Name" = { "S" = "John Doe" },
+#     "Age"          = { "N" = "56" },
+#     "Gender"       = { "S" = "Female" },
+#     "Test_Name"    = { "S" = "Blood Glucose" },
+#     "Result"       = { "S" = "Normal" },
+#     "Date"         = { "S" = "2025-03-05" },
+#     "Doctor"       = { "S" = "Dr. Williams" },
+#     "Remarks"      = { "S" = "Lifestyle changes recommended" }
+#   })
+# }
 
