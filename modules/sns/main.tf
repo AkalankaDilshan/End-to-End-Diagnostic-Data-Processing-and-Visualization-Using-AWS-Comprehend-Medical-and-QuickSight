935d2c5ac7bc027396110e1163814ca38ae463d4
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-05 22:15:44 +0530

    chor: change resources names

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -1,6 +1,6 @@
-resource "aws_sns_topic" "database_update" {
-  name              = var.topic_name
-  kms_master_key_id = "alias/aws/sns"
+resource "aws_sns_topic" "notification" {
+  name = var.notification_name
+  #kms_master_key_id = "alias/aws/sns"
 }
 
 resource "aws_sns_topic_subscription" "email" {
