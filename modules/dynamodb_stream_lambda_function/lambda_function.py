4ac6626298b8f6c18c607471cd3c6a2b7db30a55
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-05 22:17:55 +0530

    chor: optimis lambda code

--- modules/dynamodb_stream_lambda_function/lambda_function.py
+++ modules/dynamodb_stream_lambda_function/lambda_function.py
@@ -1,37 +1,39 @@
 import json
 import boto3
+import os
+
+sns_client = boto3.client('sns')
 
 def lambda_handler(event, context):
-    sns = boto3.client('sns')
-    account_id = context.invoked_function_arn.split(":")[4]
-    region = context.invoked_function_arn.split(":")[3]
-    
-    sns_topic_name = "Add_new_medical_record"
-    processed_records = []
-    
-    for record in event['Records']:
-        # Only process INSERT events (new items)
-        if record['eventName'] == 'INSERT':
-            new_image = record['dynamodb']['NewImage']
-            report_id = new_image['Report_ID']['S']
-            
-            # Publish the message to SNS
-            response = sns.publish(
-                TopicArn=f'arn:aws:sns:{region}:{account_id}:{sns_topic_name}',
-                Message=f'New Medical Report {report_id} has been added',
-                Subject='New Medical Report Added to Database'
-            )
-            
-            processed_records.append({
-                'report_id': report_id,
-                'eventName': record['eventName'],
-                'sns_response': response
-            })
-    
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": f"Processed {len(processed_records)} new medical records",
-            "processed_records": processed_records
-        })
-    }
+    try:
+        for record in event['Records']:
+            if record['eventName'] == 'INSERT':
+                new_item = record['dynamodb']['NewImage']
+                
+                #email msg
+                message = f"""
+                New item added to DynamoDB table: {json.dumps(new_item,indent=2)}
+                
+                Event details:
+                - Event ID: {record['eventID']}
+                - Event Source: {record['eventSource']}
+                - AWS Region: {record['awsRegion']}
+                """
+                
+                # send sns email
+                response = sns_client.publish(
+                    TopicArn=os.environ['SNS_TOPIC_ARN']
+                    Message=message,
+                    Subject='New Item Added to DynamoDB'
+                )
+                
+                print(f"Notification sent: {response['MessageId']}")
+        
+        return{
+            'statusCode': 200,
+            'body': json.dump('Processe DynamoDB stream records successfully')
+        }
+        
+    except Exception as e:
+        print(f"Error processing DynamoDB stream: {str(e)}")
+        raise e
