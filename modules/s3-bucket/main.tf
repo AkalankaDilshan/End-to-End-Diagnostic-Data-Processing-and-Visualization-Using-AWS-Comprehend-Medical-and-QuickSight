a635cce7d08c0568dc160a7b80522eb8f8ed4106
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-05 14:52:52 +0530

    feat: enable versioning

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -14,6 +14,23 @@ resource "aws_s3_bucket" "Medical_report_input_bucket" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "upload_bucket_versioning" {
+  bucket = aws_s3_bucket.Medical_report_input_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "upload_bucket_encryption" {
+  bucket = aws_s3_bucket.Medical_report_input_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "Medical_report_input_bucket_access" {
   bucket = aws_s3_bucket.Medical_report_input_bucket.id
 
