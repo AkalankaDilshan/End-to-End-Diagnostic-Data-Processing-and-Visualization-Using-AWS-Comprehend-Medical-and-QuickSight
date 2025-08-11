15c7d6f9cf7cf0bba121cf6a3ed4560b18d6fd2b
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-11 11:53:46 +0530

    feat:  add lambda function permission part

---
+++ modules/api-s3-lambda/main.tf
@@ -0,0 +1,25 @@
+resource "aws_lambda_function" "file_upload_function" {
+  function_name    = var.function_name
+  role             = var.role_arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "Python3.11"
+  filename         = "${path.module}/function.zip"
+  source_code_hash = filebase64sha256("${path.module}/function.zip")
+
+  environment {
+    variables = {
+      S3_BUCKET = var.s3_bucket_id
+    }
+  }
+
+  timeout     = 60
+  memory_size = 256
+}
+
+resource "aws_lambda_permission" "apligw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.file_upload_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${var.api_gateway_arn}/*/POST/upload"
+}
---
+++ modules/api-s3-lambda/variables.tf
@@ -0,0 +1,21 @@
+variable "function_name" {
+  description = "name for lambda function"
+  type        = string
+  default     = "apiFileUploadHandler"
+}
+
+
+variable "role_arn" {
+  description = "lambda iam role arn"
+  type        = string
+}
+
+variable "s3_bucket_id" {
+  description = "aws_s3_bucket.file_uploads.id"
+  type        = string
+}
+
+variable "api_gateway_arn" {
+  description = "aws_api_gateway_rest_api.file_upload_api.execution_arn"
+  type        = string
+}
