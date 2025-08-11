16f34405e9f15f32e69785d82c0c154ac40bf79c
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-11 11:19:58 +0530

    feat: finished roughly lambda function

---
+++ modules/Iam/iam_for_api_s3_lambda/main.tf
@@ -0,0 +1,50 @@
+data "aws_iam_policy_document" "lambda_policy_documet" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_police_document" "assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+
+resource "aws_iam_role" "lambda_role" {
+  name               = var.role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "${var.role_name}-lambda-policy"
+  description = "Iam policy for lambda to access s3 and logs"
+  policy      = data.aws_iam_policy_document.lambda_policy_documet.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
