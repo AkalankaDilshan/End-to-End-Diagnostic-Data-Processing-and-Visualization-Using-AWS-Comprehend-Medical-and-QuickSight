a94479ce24798e35f452f4f9016211c5115a7e68
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-11 14:09:39 +0530

    fix: gateway deployment staging issue

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -29,7 +29,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on  = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.file_upload_api.id
-  stage_name  = "prod"
 }
 
 resource "aws_api_gateway_authorizer" "cognito" {
