404432d93aa13d9ee5f55177ada79849a8fa8d37
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-11 14:51:35 +0530

    chor: comment function permission

--- modules/api-s3-lambda/main.tf
+++ modules/api-s3-lambda/main.tf
@@ -16,10 +16,10 @@ resource "aws_lambda_function" "file_upload_function" {
   memory_size = 256
 }
 
-resource "aws_lambda_permission" "apligw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.file_upload_function.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${var.api_gateway_arn}/*/POST/upload"
-}
+# resource "aws_lambda_permission" "apligw_lambda" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.file_upload_function.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${var.api_gateway_arn}/*/POST/upload"
+# }
