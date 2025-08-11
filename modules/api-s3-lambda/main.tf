b41bb05e748a992c3a97a75ea467e5731fe13f46
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-11 14:42:49 +0530

    fix: spelling mistake :apple:

--- modules/api-s3-lambda/main.tf
+++ modules/api-s3-lambda/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "file_upload_function" {
   function_name    = var.function_name
   role             = var.role_arn
   handler          = "lambda_function.lambda_handler"
-  runtime          = "Python3.11"
+  runtime          = "python3.11"
   filename         = "${path.module}/function.zip"
   source_code_hash = filebase64sha256("${path.module}/function.zip")
 
